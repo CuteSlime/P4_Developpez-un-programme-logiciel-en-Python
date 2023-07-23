140db2645b00ee82cdd536a20c8d40718ffcdb56
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-07-23 23:15:20 +0200

    update base

---
+++ main.py
@@ -0,0 +1,44 @@
+# Chaque tournoi doit contenir au moins les informations suivantes :
+# ● nom ;
+# ● lieu ;
+# ● date de début et de fin ;
+# ● nombre de tours – réglez la valeur par défaut sur 4 ;
+# ● numéro correspondant au tour actuel ;
+# ● une liste des tours ;
+# ● une liste des joueurs enregistrés ;
+# ● description pour les remarques générales du directeur du tournoi.
+
+# Match = ([joueur 1, score], [joueur 2, score])
+
+# class Tournoi():
+#     nom = ""
+#     lieu = ""
+#     date_debut =""
+#     date_fin = ""
+#     nb_tour = 4
+#     numero_tour = 0
+#     tour = [Tour1, Tour2]
+#     joueur = [Joueur1, joueur2]
+#     remarque = ""
+
+# class Tour():
+#   nom = "round 1"
+#   match = [match1, match2, match3]
+#   date_debut = ""
+#   date_fin = ""
+
+# club ?
+
+# class Joueur():
+#     nom = ""
+#     prenom = ""
+#     date_naissance = ""
+
+
+# RAPPORT
+
+#  liste de tous les joueurs par ordre alphabétique ;
+# ● liste de tous les tournois ;
+# ● nom et dates d’un tournoi donné ;
+# ● liste des joueurs du tournoi par ordre alphabétique ;
+# ● liste de tous les tours du tournoi et de tous les matchs du tour
