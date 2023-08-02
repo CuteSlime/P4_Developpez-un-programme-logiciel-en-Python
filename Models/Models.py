25a42498af20488541b9cda70fec18bb5224094a
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-02 23:48:52 +0200

    add some method

--- Models/Models.py
+++ Models/Models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Tournoi:
     def __init__(self, lieu, date_debut, date_fin, numero_tour, tour, joueur, remarque, nb_tour=4):
         self.lieu = lieu
@@ -8,14 +11,19 @@ class Tournoi:
         self.tour = tour
         self.joueur = joueur
         self.remarque = remarque
-
+    def add_joueur(self):
+        
 
 class Tour:
-    def __init__(self, nom, match, date_debut, date_fin):
+    def __init__(self, nom, match):
         self.nom = nom
         self.match = match
-        self.date_debut = date_debut
-        self.date_fin = date_fin
+        self.date_debut = datetime.now()
+        self.date_fin = "en cours"
+
+    def tour_fini(self):
+        self.date_fin = datetime.now()
+        return self.date_fin
 
 
 class Club:
@@ -30,6 +38,9 @@ class Joueur:
         self.prenom = prenom
         self.date_naissance = date_naissance
         self.club = club
+#import JSON pour Joueur, plus pour le club du joueur
+    def full_name(self):
+        return f"{self.prenom} {self.nom}"
 
 
 # class Match:
