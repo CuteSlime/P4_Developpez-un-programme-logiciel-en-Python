e1dab0e29580e0db448186c67647d66924242039
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-16 22:13:42 +0200

    try to start view

---
+++ Views/Views.py
@@ -0,0 +1,24 @@
+from Controllers.Controllers import run
+
+
+def menu_principal():
+    print("Bonjour, /nBienvenu dans l'outils de gestion de tournois./nQue souhaiter vous faire ?")
+    print("[1] gérer les tournois")
+    print("[2] gérer les joueur")
+    print("[3] gérer les club")
+    print("[4] quitter")
+
+    choix = input("choix N° :")
+    return choix
+
+
+def menu_tournois():
+    print("Menu de gestion des tournois")
+
+
+def menu_joueur():
+    print("Menu de gestion des joueurs")
+
+
+def menu_club():
+    print("Menu de gestion des clubs")
