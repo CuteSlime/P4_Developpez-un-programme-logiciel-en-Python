7d7b0a0b80e7f0e9b2ea98ae31c437fc342f90f5
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-17 17:38:50 +0200

    update, shuffle player and create match with them

--- Models/tour.py
+++ Models/tour.py
@@ -1,18 +1,16 @@
 from datetime import datetime
-import json
-
+from .joueur import list_joueurs
 from .match import Match
 # from .club import Club
 
 
-def list_tours():
-    with open('../data/tours.json', 'r', encoding='utf8') as tours_data:
-        tours_data = json.load(tours_data)
-    return tours_data
+# def list_tours():
+#     with open('../data/tours.json', 'r', encoding='utf8') as tours_data:
+#         tours_data = json.load(tours_data)
+#     return tours_data
 
 
-tours_data = list_tours()
-
+# tours_data = list_tours()
 
 class Tour:
     def __init__(self, nom):
@@ -20,45 +18,48 @@ class Tour:
         self.list_matchs = []
         self.date_debut = datetime.now().strftime('%d/%m/%Y %H:%M')
         self.date_fin = "en cours"
+        self.add_match()
 
-    def add_match(self, match):
-        self.list_matchs.append(match.match_print())
+    def add_match(self):
+        joueurs = list_joueurs().shuffle()
+        while len(joueurs) > 0:
+            self.list_matchs.append(Match(joueurs[0], joueurs[1]))
 
     def tour_fini(self):
         self.date_fin = datetime.now().strftime('%d/%m/%Y %H:%M')
         return self.date_fin
 
-    def add_to(self):
-        exist = False
-        if not isinstance(self, Tour):
-            return print("Ceci n'est pas un Tour valide")
-        for tour in tours_data:
-            if self.__dict__ == tour:
-                exist = True
-                break
-        if exist:
-            return print("Ce tour existe déjà")
-        tours_data.append(self.__dict__)
-        tour_json = json.dumps(tours_data, indent=4)
-        with open('../data/tours.json', 'w', encoding='utf8') as jsonfile:
-            jsonfile.write(tour_json)
+    # def add_to(self):
+    #     exist = False
+    #     if not isinstance(self, Tour):
+    #         return print("Ceci n'est pas un Tour valide")
+    #     for tour in tours_data:
+    #         if self.__dict__ == tour:
+    #             exist = True
+    #             break
+    #     if exist:
+    #         return print("Ce tour existe déjà")
+    #     tours_data.append(self.__dict__)
+    #     tour_json = json.dumps(tours_data, indent=4)
+    #     with open('../data/tours.json', 'w', encoding='utf8') as jsonfile:
+    #         jsonfile.write(tour_json)
 
-    def remove_from(self):
-        if not isinstance(self, Tour):
-            return ValueError("Ceci n'est pas un Tour valide")
-        if self.__dict__ in tours_data:
-            tours_data.remove(self.__dict__)
-        tour_json = json.dumps(tours_data, indent=4)
-        with open('../data/tours.json', 'w', encoding='utf8') as jsonfile:
-            jsonfile.write(tour_json)
+    # def remove_from(self):
+    #     if not isinstance(self, Tour):
+    #         return ValueError("Ceci n'est pas un Tour valide")
+    #     if self.__dict__ in tours_data:
+    #         tours_data.remove(self.__dict__)
+    #     tour_json = json.dumps(tours_data, indent=4)
+    #     with open('../data/tours.json', 'w', encoding='utf8') as jsonfile:
+    #         jsonfile.write(tour_json)
 
-    def update_from(self, original):
-        if not isinstance(self, Tour):
-            return ValueError("Ceci n'est pas un Club valide")
-        if original.__dict__ in tours_data:
-            tours_data.remove(original.__dict__)
-            tours_data.append(self.__dict__)
-            tour_json = json.dumps(tours_data, indent=4)
-        with open('../data/tours.json', 'w', encoding='utf8') as jsonfile:
-            jsonfile.write(tour_json)
-            return tours_data
+    # def update_from(self, original):
+    #     if not isinstance(self, Tour):
+    #         return ValueError("Ceci n'est pas un Club valide")
+    #     if original.__dict__ in tours_data:
+    #         tours_data.remove(original.__dict__)
+    #         tours_data.append(self.__dict__)
+    #         tour_json = json.dumps(tours_data, indent=4)
+    #     with open('../data/tours.json', 'w', encoding='utf8') as jsonfile:
+    #         jsonfile.write(tour_json)
+    #         return tours_data
