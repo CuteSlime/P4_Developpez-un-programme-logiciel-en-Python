a494cf7b258c91fd1ce21d82a7ae7b03683836f9
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-17 23:32:45 +0200

    update view

--- Views/base.py
+++ Views/base.py
@@ -1,43 +1,43 @@
+class Views:
+    def __init__(self):
+        pass
 
-def menu_principal():
-    print("Bonjour, /nBienvenu dans l'outils de gestion de tournois./nQue souhaiter vous faire ?")
-    print("[1] gérer les tournois")
-    print("[2] gérer les joueur")
-    print("[3] gérer les club")
-    print("[4] quitter")
+    def menu_principal():
+        print("Bonjour, /nBienvenu dans l'outils de gestion de tournois./nQue souhaiter vous faire ?")
+        print("[1] gérer les tournois")
+        print("[2] gérer les joueur")
+        print("[3] gérer les club")
+        print("[4] quitter")
 
-    choix = input("choix N° :")
-    return choix
+        choix = input("choix N° :")
+        return choix
 
+    def menu_tournois():
+        print("Menu de gestion des tournois")
+        print("[1] créer un tournoi")
+        print("[2] modifier un tournoi")
+        print("[3] suprimer un tournoi")
+        print("[4] retour au menu principal")
 
-def menu_tournois():
-    print("Menu de gestion des tournois")
-    print("[1] créer un tournoi")
-    print("[2] modifier un tournoi")
-    print("[3] suprimer un tournoi")
-    print("[4] retour au menu principal")
+        choix = input("choix N° :")
+        return choix
 
-    choix = input("choix N° :")
-    return choix
+    def menu_club():
+        print("Menu de gestion des clubs")
+        print("[1] créer un club")
+        print("[2] modifier un club")
+        print("[3] suprimer un club")
+        print("[4] retour au menu principal")
 
+        choix = input("choix N° :")
+        return choix
 
-def menu_club():
-    print("Menu de gestion des clubs")
-    print("[1] créer un club")
-    print("[2] modifier un club")
-    print("[3] suprimer un club")
-    print("[4] retour au menu principal")
+    def menu_joueur():
+        print("Menu de gestion des joueurs")
+        print("[1] créer un joueur")
+        print("[2] modifier un joueur")
+        print("[3] suprimer un joueur")
+        print("[4] retour au menu principal")
 
-    choix = input("choix N° :")
-    return choix
-
-
-def menu_joueur():
-    print("Menu de gestion des joueurs")
-    print("[1] créer un joueur")
-    print("[2] modifier un joueur")
-    print("[3] suprimer un joueur")
-    print("[4] retour au menu principal")
-
-    choix = input("choix N° :")
-    return choix
+        choix = input("choix N° :")
+        return choix
