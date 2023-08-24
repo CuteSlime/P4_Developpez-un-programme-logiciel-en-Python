7cb2f93755cdd082e7ddc29832da8a7291db4a15
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-24 12:09:26 +0200

    delete old method (now in database.py)

--- models/joueur.py
+++ models/joueur.py
@@ -11,37 +11,6 @@ class Joueur:
     def full_name(self):
         return f"{self.prenom} {self.nom}"
 
-    # def add_to(self):
-    #     joueurs_database = database_access("joueurs", Joueur, "r")
-    #     exist = False
-    #     if not isinstance(self, Joueur):
-    #         return print("Ceci n'est pas un Joueur valide")
-    #     for joueur in joueurs_database:
-    #         if self.__dict__ == joueur:
-    #             exist = True
-    #             break
-    #     if exist:
-    #         return print("Ce joueur existe déjà")
-    #     joueurs_database.append(self.__dict__)
-    #     database_access("joueurs", Joueur, "w", joueurs_database)
-
-    # def remove_from(self):
-    #     joueurs_database = database_access("joueurs", Joueur, "r")
-    #     if not isinstance(self, Joueur):
-    #         return ValueError("Ceci n'est pas un Joueur valide")
-    #     if self.__dict__ in joueurs_database:
-    #         joueurs_database.remove(self.__dict__)
-    #         database_access("joueurs", Joueur, "w", joueurs_database)
-
-    # def update_from(self, original):
-    #     joueurs_database = database_access("joueurs", Joueur, "r")
-    #     if not isinstance(self, Joueur):
-    #         return ValueError("Ceci n'est pas un Joueur valide")
-    #     if original.__dict__ in joueurs_database:
-    #         joueurs_database.remove(original.__dict__)
-    #         joueurs_database.append(self.__dict__)
-    #         database_access("joueurs", Joueur, "w", joueurs_database)
-
     def __str__(self):
         return f"{self.full_name()} Née le : {self.date_naissance} {self.score} {self.club}"
 
--- models/tournois.py
+++ models/tournois.py
@@ -25,41 +25,6 @@ class Tournoi():
         self.list_joueurs = []
         self.remarque = ""
 
-    def add_to(self):
-        exist = False
-        if not isinstance(self, Tournoi):
-            return print("Ceci n'est pas un Tournoi valide")
-        for tournoi in tournois_database:
-            if self.__dict__ == tournoi:
-                exist = True
-                break
-        if exist:
-            return print("Ce tournoi existe déjà")
-        tournois_database.append(self.__dict__)
-        tournoi_json = json.dumps(tournois_database, indent=4)
-        with open('./data/tournois.json', 'w', encoding='utf8') as jsonfile:
-            jsonfile.write(tournoi_json)
-
-    def remove_from(self):
-        if not isinstance(self, Tournoi):
-            return ValueError("Ceci n'est pas un Tournoi valide")
-        if self.__dict__ in tournois_database:
-            tournois_database.remove(self.__dict__)
-        tournoi_json = json.dumps(tournois_database, indent=4)
-        with open('./data/tournois.json', 'w', encoding='utf8') as jsonfile:
-            jsonfile.write(tournoi_json)
-
-    def update_from(self, original):
-        if not isinstance(self, Tournoi):
-            return ValueError("Ceci n'est pas un Tournoi valide")
-        if original.__dict__ in tournois_database:
-            tournois_database.remove(original.__dict__)
-            tournois_database.append(self.__dict__)
-            tournoi_json = json.dumps(tournois_database, indent=4)
-        with open('./data/tournois.json', 'w', encoding='utf8') as jsonfile:
-            jsonfile.write(tournoi_json)
-            return tournois_database
-
     def add_joueur(self, joueur):
         self.list_joueurs.append(joueur)
         print(self.list_joueurs)
