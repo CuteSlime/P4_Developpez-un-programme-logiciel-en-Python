0f6abb54d516cbf6f3dbaac174359dd0b4665670
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-25 10:07:27 +0200

    convert Json to object after import, and reverse for export

--- models/database.py
+++ models/database.py
@@ -1,55 +1,65 @@
 import json
-from json import JSONEncoder
 
 
-class Encoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__
-
-
-def database_access(database_name, object_class, access_type, *objects_list):
+def database_access(database_name, object_class, access_type, *dict_list):
     access_type = str(access_type)
 
     if access_type == "r":
-        objects_list2 = json.load(
+        objects_list = []
+        database = json.load(
             open('./data/' + str(database_name) + '.json', 'r', encoding='utf8'))
-        # print("\33[93m", database, "\33[0m")
-        # for obj2 in database:
-        #     objects_list.append(object_class(**obj2))
-        return objects_list2
+        print("\33[93m", database, "\33[0m")
+        for database_object in database:
+            objects_list.append(object_class(**database_object))
+        return objects_list
     elif access_type == "w":
-        json.dump(objects_list, open('./data/' + str(database_name) +
-                                     '.json', 'w', encoding='utf8'), indent=4, cls=Encoder)
+        json.dump(dict_list, open(
+            './data/' + str(database_name) + '.json', 'w', encoding='utf8'), indent=4)
         database_access(database_name, object_class, "r")
 
 
 def add_to_database(self, objects_list, database_name, object_class):
+    '''si l'objet et bien de la bonne classe et n'est pas encore dans la list d'objet'''
+
     exist = False
     if not isinstance(self, object_class):
         return print("Ceci n'est pas un ", str(database_name), " valide")
     for obj in objects_list:
-        if self.__dict__ == obj:
+        if self.__dict__ == obj.__dict__:
             exist = True
             break
     if exist:
         return print("Ce ", str(database_name), " existe déjà")
-    objects_list.append(self.__dict__)
-    print("\33[94m", objects_list, "\33[00m")
-    database_access(database_name, object_class, "w", objects_list)
+
+    '''ajoute l'objet à la list puis convertie cette list d'objet en list de dictionnaire'''
+    objects_list.append(self)
+    dict_list = []
+    for obj in objects_list:
+        dict_list.append(obj.__dict__)
+    print("\33[94m", dict_list, "\33[00m")
+    database_access(database_name, object_class, "w", *dict_list)
 
 
 def remove_from_database(self, objects_list, database_name, object_class):
     if not isinstance(self, object_class):
         return ValueError("Ceci n'est pas un ", str(database_name), " valide")
-    if self.__dict__ in objects_list:
-        objects_list.remove(self.__dict__)
-        database_access(database_name, object_class, "w", objects_list)
+    if self in objects_list:
+        objects_list.remove(self)
+        dict_list = []
+    for obj in objects_list:
+        obj = obj.__dict__
+        dict_list.append(obj)
+    database_access(database_name, object_class, "w", dict_list)
 
 
 def update_database(self, original, objects_list, database_name, object_class):
     if not isinstance(self, object_class):
         return ValueError("Ceci n'est pas un ", str(database_name), " valide")
-    if original.__dict__ in objects_list:
-        objects_list.remove(original.__dict__)
-        objects_list.append(self.__dict__)
-        database_access(database_name, object_class, "w", objects_list)
+    if original in objects_list:
+        objects_list.remove(original)
+        objects_list.append(self)
+        dict_list = []
+        for obj in objects_list:
+            obj = obj.__dict__
+            dict_list.append(obj)
+    database_access(database_name, object_class, "w", dict_list)
