fc036a327724a4716cf6835baa773fc9e9409a43
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-29 16:38:29 +0200

    remove print, add * to parameter

--- models/database.py
+++ models/database.py
@@ -8,7 +8,6 @@ def database_access(database_name, object_class, access_type, *dict_list):
         objects_list = []
         database = json.load(
             open('./data/' + str(database_name) + '.json', 'r', encoding='utf8'))
-        print("\33[93m", database, "\33[0m")
         for database_object in database:
             objects_list.append(object_class(**database_object))
         return objects_list
@@ -36,30 +35,31 @@ def add_to_database(self, objects_list, database_name, object_class):
     dict_list = []
     for obj in objects_list:
         dict_list.append(obj.__dict__)
-    print("\33[94m", dict_list, "\33[00m")
     database_access(database_name, object_class, "w", *dict_list)
 
 
 def remove_from_database(self, objects_list, database_name, object_class):
     if not isinstance(self, object_class):
         return ValueError("Ceci n'est pas un ", str(database_name), " valide")
-    if self in objects_list:
-        objects_list.remove(self)
-        dict_list = []
+
+    for obj in objects_list:
+        if self.__dict__ == obj.__dict__:
+            objects_list.remove(obj)
+    dict_list = []
     for obj in objects_list:
         obj = obj.__dict__
         dict_list.append(obj)
-    database_access(database_name, object_class, "w", dict_list)
+    database_access(database_name, object_class, "w", *dict_list)
 
 
 def update_database(self, original, objects_list, database_name, object_class):
     if not isinstance(self, object_class):
         return ValueError("Ceci n'est pas un ", str(database_name), " valide")
-    if original in objects_list:
-        objects_list.remove(original)
-        objects_list.append(self)
-        dict_list = []
-        for obj in objects_list:
-            obj = obj.__dict__
-            dict_list.append(obj)
-    database_access(database_name, object_class, "w", dict_list)
+    for obj in objects_list:
+        if original.__dict__ == obj.__dict__:
+            objects_list[objects_list.index(obj)] = self
+    dict_list = []
+    for obj in objects_list:
+        obj = obj.__dict__
+        dict_list.append(obj)
+    database_access(database_name, object_class, "w", *dict_list)
