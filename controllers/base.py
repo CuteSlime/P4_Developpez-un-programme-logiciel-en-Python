92e6c0076bd843d769e9301f6c9ace322621ebff
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-29 17:06:56 +0200

    initialise tournoi

--- controllers/base.py
+++ controllers/base.py
@@ -5,7 +5,7 @@ from models.tour import Tour
 # from models.club import Club
 from models.joueur import Joueur
 from models.database import database_access, add_to_database, remove_from_database, update_database
-list_tournois = [database_access("joueurs", Joueur, "r")]
+list_tournois = [database_access("tournois", Tournoi, "r")]
 list_tours = []
 list_matchs = []
 list_joueurs = database_access("joueurs", Joueur, "r")
@@ -16,7 +16,7 @@ class Controller:
     '''Main controller'''
 
     def __init__(self, view):
-        # self.tournois = Tournoi.list_tournois()
+        self.tournois = list_tournois
         # self.matchs = Match.list_matchs()
         # self.clubs = Club.list_clubs()
         self.joueurs = list_joueurs
@@ -63,17 +63,18 @@ class Controller:
     #         Match.play_match()
 
     def test(self):
-        print("\33[93m", list_joueurs, "\33[00m")
-        joueur2 = Joueur("Tite", "Phillipe", "14 Mars 2001",
-                         club="not an actor")
-        joueur4 = Joueur("Touite", "Phillipe",
-                         "14 Mars 2001", club="not an actor")
-        joueur3 = Joueur("Touuite", "Phillipe",
-                         "14 Mars 2001", club="not an actor")
-        joueur1 = Joueur("Toucuite", "Phillipe",
-                         "14 Mars 2001", club="not an actor")
-
-        add_to_database(joueur1, list_joueurs, "joueurs", Joueur)
-        # remove_from_database(joueur1, list_joueurs, "joueurs", Joueur)
-        update_database(joueur2, joueur1, list_joueurs, "joueurs", Joueur)
-        print("\33[93m", list_joueurs, "\33[00m")
+        tournoi = Tournoi("Biblo", "paris", "24/12/23", "25/12/23", 3)
+        # print("\33[93m", list_joueurs, "\33[00m")
+        # joueur2 = Joueur("Tite", "Phillipe", "14 Mars 2001",
+        #                  club="not an actor")
+        # joueur4 = Joueur("Touite", "Phillipe",
+        #                  "14 Mars 2001", club="not an actor")
+        # joueur3 = Joueur("Touuite", "Phillipe",
+        #                  "14 Mars 2001", club="not an actor")
+        # joueur1 = Joueur("Toucuite", "Phillipe",
+        #                  "14 Mars 2001", club="not an actor")
+        add_to_database(tournoi, list_tournois, "tournois", Tournoi)
+        # add_to_database(joueur1, list_joueurs, "joueurs", Joueur)
+        # # remove_from_database(joueur1, list_joueurs, "joueurs", Joueur)
+        # update_database(joueur2, joueur1, list_joueurs, "joueurs", Joueur)
+        # print("\33[93m", list_joueurs, "\33[00m")
