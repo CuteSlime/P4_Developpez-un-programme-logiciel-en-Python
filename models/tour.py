cc0426f990f7f906800008554b920c22476c9ce2
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-08-30 22:30:47 +0200

    move start date to method, update add_match

--- models/tour.py
+++ models/tour.py
@@ -8,17 +8,19 @@ class Tour:
     def __init__(self, nom):
         self.nom = nom
         self.list_matchs = []
-        self.date_debut = datetime.now().strftime('%d/%m/%Y %H:%M')
+        self.date_debut = ""
         self.date_fin = "en cours"
         self.add_match()
 
-    def add_match(self):
-        joueurs = list_joueurs()
+    def add_match(self, list_joueurs_tournois):
+        joueurs = list_joueurs_tournois
         shuffle(joueurs)
-
         while len(joueurs) > 1:
             self.list_matchs.append(Match(joueurs[0], joueurs[1]))
             del joueurs[0:2]
+
+    def start_tour(self):
+        self.date_debut = datetime.now().strftime('%d/%m/%Y %H:%M')
 
     def tour_fini(self):
         self.date_fin = datetime.now().strftime('%d/%m/%Y %H:%M')
