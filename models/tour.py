40524be5bb792b2b86cc9f0b27177e10ec8865f4
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-09-29 11:48:59 +0200

    update add_match(), add function to reduce complexity

--- models/tour.py
+++ models/tour.py
@@ -3,6 +3,48 @@ from random import shuffle, random
 from datetime import datetime
 
 
+def sort_by_score(players_to_sort):
+    i = 0
+    list_joueurs = []
+    not_sorted_joueur = players_to_sort[:]
+    while i < len(players_to_sort):
+        score = -1
+
+        meilleur_joueur = not_sorted_joueur[0]
+        for joueur in not_sorted_joueur:
+            if joueur.score > score:
+                score = joueur.score
+                meilleur_joueur = joueur
+        list_joueurs.append(meilleur_joueur)
+        not_sorted_joueur.remove(meilleur_joueur)
+        i += 1
+    return list_joueurs
+
+
+def already_played_together(list_joueurs, list_previous_match):
+    i = 1
+    sorted_joueurs = []
+    not_sorted_joueurs = list_joueurs[:]
+    while len(not_sorted_joueurs) > 1:
+        not_sorted = True
+        for round in list_previous_match:
+            last_match_player_1 = round[0][0]
+            last_match_player_2 = round[1][0]
+
+            if (not_sorted_joueurs[0].id in (last_match_player_1["id"], last_match_player_2["id"])
+                    and
+                    not_sorted_joueurs[i].id in (last_match_player_1["id"], last_match_player_2["id"])):
+
+               
+        sorted_joueurs.append(not_sorted_joueurs[0])
+        sorted_joueurs.append(not_sorted_joueurs[i])
+        not_sorted_joueurs.remove(not_sorted_joueurs[i])
+        not_sorted_joueurs.remove(not_sorted_joueurs[0])
+        i = 1
+
+    return sorted_joueurs
+
+
 class Tour:
     def __init__(self, nom, **kwargs):
         self.nom = nom
@@ -21,50 +63,8 @@ class Tour:
         if is_first_match:
             shuffle(joueurs)
         else:
-            i = 0
-            list_joueurs = []
-            not_sorted_joueur = joueurs[:]
-            while i < len(joueurs):
-                score = -1
 
-                meilleur_joueur = not_sorted_joueur[0]
-                for joueur in not_sorted_joueur:
-                    if joueur.score > score:
-                        score = joueur.score
-                        meilleur_joueur = joueur
-                list_joueurs.append(meilleur_joueur)
-                not_sorted_joueur.remove(meilleur_joueur)
-                i += 1
-            i = 0
-            while i < len(list_joueurs):
-                try:
-                    list_joueurs[i+1]
-                except IndexError:
-                    i += 1
-                    break
-
-                if list_joueurs[i].score == list_joueurs[i + 1].score:
-                    for round in list_previous_match:
-
-                        for match in round:
-                            if list_joueurs[i].__dict__ in match:
-                                print(
-                                    list_joueurs[i].full_name(),
-                                    " [------joueur------] ",
-                                    list_joueurs[i + 1].full_name()
-                                )
-                                if list_joueurs[i + 1].__dict__ in match:
-                                    print(
-                                        list_joueurs[i].full_name(),
-                                        " [--VS--] ",
-                                        list_joueurs[i + 1].full_name()
-                                    )
-                                    list_joueurs.insert(
-                                        i + 1, list_joueurs.pop(i))
-                    i += 1
-                else:
-                    i += 1
-            joueurs = list_joueurs
+            joueurs = already_played_together(sort_by_score(joueurs), list_previous_match)
 
         groupes = (joueurs[i:i+2] for i in range(0, len(joueurs), 2))
         for groupe in groupes:
